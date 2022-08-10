0dd1ab9976cf9d85a54f63a4dac339d3ea281dd2
James-Xianglovemeng <13611687113@sina.cn>
2022-08-10 22:30:06 +0800

    gitee commit

--- git-test/src/main/java/com/XiangStone/git/GitTest.java
+++ git-test/src/main/java/com/XiangStone/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("Hello,git----hot-fix22222");
         System.out.println("ssh commit success!!!");
         System.out.println("update code by GitHub~");
+        System.out.println("update code by gitee");
     }
 }
