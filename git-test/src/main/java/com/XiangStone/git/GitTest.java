903f4e823781032753ff8ae38e939bdb793ebfe7
James-Xianglovemeng <13611687113@sina.cn>
2022-08-10 18:16:54 +0800

    first commit

---
+++ git-test/src/main/java/com/XiangStone/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.XiangStone.git;
+
+/**
+ * @author Xiang Stone
+ * @create 2022-08-10 18:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello,git!!!");
+    }
+}
